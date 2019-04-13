55d9a4801f00bc1a20e47f0b26fc4137edbe8e4b
ningjianhao <375332835@qq.com>
2019-04-13 17:28:34 +0800

    init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,161 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div class="hello">
+        <div class="banner">
+            <div class="banner-item">
+                <p>TTBTC Bitcoin investment fund</p>
+                <p>下载TTpay手机客户端，参与基金理财</p>
+                <p>
+                    <button>Android下载</button>
+                </p>
+            </div>
+        </div>
+        <div class="advertising1">
+            <div class="advertising1-item">
+                <div>
+                    <p>Fund raising period: April 15, 2019 to April 30, 2019 </p>
+                    <p>Fund cycl:6 months</p>
+                </div>
+                <div>
+                    <button>coming soon</button>
+                </div>
+            </div>
+        </div>
+        <div class="fund-top">
+            <div class="fund-top-text">
+                <div class="fund-top-text-title">
+                    <p>TTBTC Bitcoin </p>
+                    <p>Investment Fund</p>
+                    <p class=""></p>
+                </div>
+                <div class="fund-top-text-container">
+                    <p>Bitcoin is the greatest asset in human history Accumulating bitcoin is the most correct strategy at the moment</p>
+                    <div>
+                        <pre>
+                            If you are eager to earn more bitcoin If you expect a solid BTC return
+                        </pre>
+                        <pre>
+                            if you expect a solid BTC return
+                        </pre>
+                        <pre>
+                            if you want to hedge the risk of currency price fluctuations
+                        </pre>
+                    </div>
+                </div>
+            </div>
+            <div class="fund-top-img"></div>
+        </div>
+        <div class="advertising2"></div>
+        <div class="fund-middle"></div>
+        <div class="advertising3"></div>
+        <div class="fund-bottom"></div>
+        <div class="footer"></div>
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+    export default {
+        name: 'HelloWorld',
+
+    }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+    button{
+        color:#fff;
+        border:0;
+        outline: none;
+        cursor: pointer;
+    }
+    .banner{
+        background: url("../assets/banner.jpg") no-repeat 0 -100px;
+        background-size: 100%;
+        height:618px;
+        text-align: center;
+        padding-top:250px;
+        line-height: 80px;
+    }
+    .banner .banner-item p:first-child{
+        font-size:60px;
+    }
+    .banner .banner-item p:nth-child(2){
+        font-size:28px;
+    }
+    .banner .banner-item p:last-child button{
+        font-size:16px;
+        display: inline-block;
+        width: 153px;
+        height:44px;
+        background: #DD2C41;
+        border-radius: 10px;
+        text-align: center;
+        line-height: 44px;
+
+    }
+
+
+
+    .advertising1{
+        height:180px;
+        background:#DD2C41;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .advertising1 .advertising1-item{
+        width:1080px;
+        height:180px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+    .advertising1 .advertising1-item > div:first-child{
+        font-size:28px;
+        color:#fff;
+        line-height: 40px;
+    }
+    .advertising1 .advertising1-item > div:last-child button{
+        width:183px;
+        height:48px;
+        background:#fff;
+        font-size:18px;
+        color:#0F0F0F;
+    }
+
+
+    .fund-top{
+        height:760px;
+        display: flex;
+        justify-content: space-between;
+    }
+    .fund-top-text{
+        display: flex;
+        flex:1;
+        flex-direction: column;
+        justify-content: center;
+        align-items: left;
+        text-indent: 360px;
+    }
+    .fund-top-text-title{
+        font-size:56px;
+        color:#282828;
+    }
+    .fund-top-text-title p{
+        font-weight: 900;
+    }
+    .fund-top-text-container{}
+    .fund-top-img{
+        width:749px;
+
+        background:url("../assets/ttbtc.jpg") no-repeat 0 120px;
+        background-size:100%;
+    }
+
+
+
+
+    .advertising2{}
+    .fund-middle{}
+    .advertising3{}
+    .fund-bottom{}
+    .footer{}
 </style>
